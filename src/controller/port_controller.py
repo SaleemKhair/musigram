5acd96ed61d70f04b34a35a0d47064ca0833dcd1
saleem <saleemkhair@gmail.com>
2022-04-04 03:22:33 +0300

    docs

--- src/controller/port_controller.py
+++ src/controller/port_controller.py
@@ -1,6 +1,6 @@
 from serialization.tone_clock_serializer import ToneClockSerializer
 
-# TODO Refactor: Naming And Design
+# TODO Refactor: Naming, Design
 # The name is not accurate, It acts more as a monitor than a controller,
 # Maybe PortInputMonitor(?)
 class PortController:
