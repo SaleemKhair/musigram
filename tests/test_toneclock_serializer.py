fa193506dca176a5d06e52d7b407e456b35b0c4f
saleem <saleemkhair@gmail.com>
2022-04-11 08:18:04 +0300

    refactor: tests

--- tests/test_toneclock_serializer.py
+++ tests/test_toneclock_serializer.py
@@ -1,8 +1,8 @@
-from serialization.tone_clock_serializer import ToneClockSerializer
+from diagram.serialization import ClockSerializer
 from mido import MidiFile
 import unittest
 
 class TestToneClockSerializer(unittest.TestCase):
     def test_serialize(self):
         for track in MidiFile('./data/Nothing Else Matters - Metallica.mid').tracks:            
-            print(track.name)
+            assert len([ClockSerializer().serialize_message(i, msg) for i, msg in enumerate(track)]) == len(track)
