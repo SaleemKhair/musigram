fe881664e2c82ce5ce9ec9ed7ed8dee4f0e99f57
saleem <saleemkhair@gmail.com>
2022-04-14 06:27:32 +0300

    feat: serialization support channel separation

--- src/diagram/serialization/__init__.py
+++ src/diagram/serialization/__init__.py
@@ -1,39 +1,34 @@
-from mido import MidiFile
+from mido import MidiFile, Message
 import logging
 from ..model import Clock
 from diagram import Serializer
-import threading
+from typing import Dict
 
 LOG = logging.getLogger(__package__)
 
 class ClockSerializer(Serializer):
     """
     A Serializer that parses MIDI messages to Clock instances.
-
-    ...
-
-    Attributes
-    ----------
-    __chord: threading.local
-        .clock: Clock
-            A stateful thread-safe instance of Clock that is cached during the stream to keep track if multiple notes
-            are played simultaneously.
     """
     def __init__(self) -> None:
-        self.__chord = threading.local()
-        self.__chord.clock = None
+        self.__chords: Dict[int,Clock] = dict()
+        self.clock: Clock = None
 
-    def serialize_message(self, idx: int, message):
-        if self.__chord.clock is None: # create and cache a new instance
-            self.__chord.clock = Clock(
+    def serialize_message(self, idx: int, message: Message):
+        if self.__chords.get(message.channel) is None: # create and cache a new instance
+            self.clock =  Clock(
                 channel=message.channel,
                 idx=idx)
+            self.__chords[message.channel] = self.clock
         else:
             if message.type == 'note_off': # remove note from cached instance and return state
-                self.__chord.clock.remove(message.note)
+                self.clock.remove(message.note)
             if message.type == 'note_on': # add note to cached instance and return state
-                self.__chord.clock.append(message.note)
-        return self.__chord.clock
+                self.clock.append(message.note)
+        LOG.debug(repr(self.clock))
+        LOG.debug(repr(message))
+        return self.clock
+
 
     def play(self, file_path):
         with open(file=file_path, mode='rb') as midi_file:
